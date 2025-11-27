29034aeaad55a71379dad6d5c997c5ae519cf33e
xaoscience <69734795+xaoscience@users.noreply.github.com>
2025-11-27 11:37:28 +0100

    dependabot-automerge: rm decentralisation

--- .github/workflows/dependabot-automerge-reusable/scripts/list-dependabot-repos.sh
+++
@@ -1,5 +0,0 @@
-gh repo list --limit 500 --json name --jq '.[].name' | while read repo; do
-  if gh api "repos/$(gh api user -q .login)/$repo/contents/.github/dependabot.yml" >/dev/null 2>&1; then
-    echo "$repo"
-  fi
-done
